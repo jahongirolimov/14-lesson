e036f8fc299f4461021a13f802102d7534f94d5c
JOHON-DEV <machoman4701@gmail.com>
2024-01-30 07:13:39 +0500

    14-lesson

---
+++ index.js
@@ -0,0 +1,22 @@
+// const students=[
+//     {name:"QodirAli",percent:96},
+//     {name:"JAson",percent:78},
+//     {name:"Nodir",percent:75},
+//     {name:"anna",percent:96},
+//     {name:"anora",percent:65},
+// ];
+// let arr = students.filter((el)=>el.percent<84&&el.percent>=65)
+// console.log(arr)
+// const students=[
+//     {name:"QodirAli",percent:96},
+//     {name:"JAson",percent:78},
+//     {name:"Nodir",percent:75},
+//     {name:"anna",percent:96},
+//     {name:"anora",percent:65},
+// ];
+// let arr = students.filter((el)=>el.name.length>5)
+// console.log(arr)
+// /////////////////////////////////
+
+
+let elFrom=document.querySelector(".")
